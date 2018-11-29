50440c5c4b9065a7bfd295de2fe7491f19800507
jovany <cap.vany@gmail.com>
2018-11-28 17:22:59 -0800

    removed unused metrics app

--- src/MetricsApp.java
+++
@@ -1,9 +0,0 @@
-public class MetricsApp {
-   static IMetrics inst = new Metrics();
-   static Metrics runner= new Metrics();
-    public static void main(String[] args){
-
-        runner.main(args);
-
-    }
-}
